238aa14dea9da4c19e474349d3073a79878df310
Navid-Sharifi <navid0523@gmail.com>
2025-06-30 11:29:43 +0100

    chore: add dl assets

--- src/components/HttpAssistant/UseHttpAssistant.ts
+++ src/components/HttpAssistant/UseHttpAssistant.ts
@@ -2,40 +2,61 @@
 import { toast } from "react-toastify"
 import { Http } from "./Utilities/Http"
 import { useHttpClient } from "./Utilities/useHttpClient"
+import axios, { AxiosResponse } from "axios"
 
 
 export const UseHttpAssistant = () => {
 
     var { isLoading, send } = useHttpClient()
 
-    const SendRequest: (method: Http, url: string, data?: any, responseType?: 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream' | 'formdata', noErrorMessage?: boolean, baseURL?: string | undefined) => any
-        = async (method: Http, url: string, data?: any, responseType?: 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream' | 'formdata', noErrorMessage: boolean = false, baseURL?: string | undefined) => {
+    const SendRequest: (method: Http, url: string, data?: any, responseType?: 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream' | 'formdata', noErrorMessage?: boolean, baseURL?: string | undefined, isPublicFiles?: boolean) => any
+        = async (method: Http, url: string, data?: any, responseType?: 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream' | 'formdata', noErrorMessage: boolean = false, baseURL?: string | undefined, isPublicFiles?: boolean) => {
 
-            var { errorMessage, response, dontShowMessage, } = await send({
-                baseURL: baseURL,
-                method: method,
-                url: url,
-                data: data,
-                timeout: 5 * 60 * 1000
-            },
-                responseType
-            )
 
-            if (errorMessage && !noErrorMessage && !dontShowMessage) {
-                toast.error(errorMessage, {
-                    position: "bottom-center",
-                    autoClose: 5000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                });
+            if (isPublicFiles) {
 
-                return undefined
+                try {
+                    const response: AxiosResponse<Blob> = await axios.get(url, {
+                        responseType: 'blob',
+                        headers: {
+                            'Accept': 'application/octet-stream',
+                        }
+                    });
+                    return response.data;
+                } catch (error) {
+                    console.error('Download failed:', error);
+                    throw error;
+                }
+
+            } else {
+
+                var { errorMessage, response, dontShowMessage, } = await send({
+                    baseURL: baseURL,
+                    method: method,
+                    url: url,
+                    data: data,
+                    timeout: 5 * 60 * 1000
+                },
+                    responseType
+                )
+
+                if (errorMessage && !noErrorMessage && !dontShowMessage) {
+                    toast.error(errorMessage, {
+                        position: "bottom-center",
+                        autoClose: 5000,
+                        hideProgressBar: true,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    });
+
+                    return undefined
+                }
+                return response
             }
-            return response
+
         }
 
     const SendRequestWithError: (method: Http, url: string, data?: any, responseType?: 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream' | 'formdata', noErrorMessage?: boolean, baseURL?: string | undefined) => any
@@ -80,9 +101,9 @@ export const UseHttpAssistant = () => {
             return SendRequest(Http.DELETE, url, data, undefined, noErrorMessage, baseURL)
         }
 
-    const GetFile: (url: string, data?: any, noErrorMessage?: boolean, baseURL?: string | undefined) => any
-        = async (url: string, data?: any, noErrorMessage?: boolean, baseURL?: string | undefined) => {
-            return SendRequest(Http.GET, url, data, "blob", noErrorMessage, baseURL)
+    const GetFile: (url: string, data?: any, noErrorMessage?: boolean, baseURL?: string | undefined, isPublicFiles?: boolean) => any
+        = async (url: string, data?: any, noErrorMessage?: boolean, baseURL?: string | undefined, isPublicFiles?: boolean) => {
+            return SendRequest(Http.GET, url, data, "blob", noErrorMessage, baseURL, isPublicFiles)
         }
 
     const PostFile: (url: string, data?: any, noErrorMessage?: boolean, baseURL?: string | undefined) => any
